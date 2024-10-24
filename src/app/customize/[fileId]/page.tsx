78910d95c572aad878aa9337c97aa5a4b55313dc
achyutem <achyutem.ad@gmail.com>
2024-10-24 18:55:53 +0530

    abilty to favourite a meme

--- src/app/customize/[fileId]/page.tsx
+++ src/app/customize/[fileId]/page.tsx
@@ -1,11 +1,13 @@
 import { unstable_noStore } from "next/cache";
 import imagekit from "@/lib/imagekit";
 import { CustomizePanel } from "./customizePanel";
+import { getFavoriteMeme } from "./loader";
 
 const CustomizePage = async ({ params }: { params: { fileId: string } }) => {
   unstable_noStore();
 
   const file = await imagekit.getFileDetails(params.fileId);
+  const isFavorited = await getFavoriteMeme(params.fileId);
 
   return (
     <div className="container mx-auto space-y-8 py-8 px-4">
@@ -13,7 +15,9 @@ const CustomizePage = async ({ params }: { params: { fileId: string } }) => {
         file={{
           filePath: file.filePath,
           name: file.name,
+          fileId: file.fileId,
         }}
+        isFavorited={isFavorited}
       />
     </div>
   );
